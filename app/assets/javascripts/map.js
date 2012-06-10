7c478206766d76208ed504d757ba61b4f5055c08
Wylie Conlon <wylieconlon@gmail.com>
2012-06-10 12:58:56 -0700

    Style tweaks

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -13,7 +13,7 @@ $(function() {
 	infoWindow = new google.maps.InfoWindow({});
 
 	map.loadFromKML({
-		url: 'http://internlunch.herokuapp.com/geofeed/5',
+		url: 'http://internlunch.herokuapp.com/geofeed/6',
 		suppressInfoWindows: true,
 		events: {
 			click: function(point){
