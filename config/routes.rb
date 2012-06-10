f4504a0ed30531308f59c1495e2c9f6e5c15a496
Wylie Conlon <wylieconlon@gmail.com>
2012-06-09 23:03:51 -0700

    Add autocomplete function, but no routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Internlunch::Application.routes.draw do
   resources :companies
+  
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
   #  get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
