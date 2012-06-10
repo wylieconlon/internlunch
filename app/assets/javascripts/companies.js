fa5cc5b40e608fd27d644a0ed9356fd33d4aa5ae
Wylie Conlon <wylieconlon@gmail.com>
2012-06-10 02:38:47 -0700

    Can add extra addresses for companies

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -19,16 +19,32 @@ $(function() {
 
 	console.log("companies loaded", company_names);
 
-	$('#company_address_confirm').hide();
+	$('#company_address_confirm').hide()
+		.click(function() {
+			var prev = $('#company_address').val();
+			
+			$('<input />', {
+				'type': 'text',
+				'id': 'company_address',
+				'placeholder': 'What is your work address?'
+			}).replaceAll('#company_address').focus();
+
+			$('#company_address_confirm').hide();
+			$('#company_address_display').text('').hide();
+
+			return false;
+		});
 	$('#company_address_display').hide();
 
 	$('#company_name').autocomplete({
 		source: company_names,
 		select: function(e, ui) {
-			console.log(ui.item, $.fx);
+			console.log(ui.item);
 			
 			$('#company_address_confirm').show();
 			$('#company_address_display').text(ui.item.address).show();
+
+			$('#company_address').val(ui.item.address);
 		}
 	})
 	.data("autocomplete")._renderItem = function(ul, item) {
