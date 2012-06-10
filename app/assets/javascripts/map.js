53b4c20bc2a6a06fc0bd92505d4f4c35a2a139a4
Wylie Conlon <wylieconlon@gmail.com>
2012-06-10 12:43:45 -0700

    Update cache buster?

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -13,7 +13,7 @@ $(function() {
 	infoWindow = new google.maps.InfoWindow({});
 
 	map.loadFromKML({
-		url: 'http://internlunch.herokuapp.com/geofeed/3',
+		url: 'http://internlunch.herokuapp.com/geofeed/4',
 		suppressInfoWindows: true,
 		events: {
 			click: function(point){
