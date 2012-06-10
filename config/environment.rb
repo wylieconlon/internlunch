ec20570c78975363dc7252768dee13e877679dad
Wylie Conlon <wylieconlon@gmail.com>
2012-06-09 22:07:26 -0700

    Give register some html, fix rename errors

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-Internlunch2::Application.initialize!
+Internlunch::Application.initialize!
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Internlunch2::Application.config.session_store :cookie_store, key: '_internlunch2_session'
+Internlunch::Application.config.session_store :cookie_store, key: '_internlunch2_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
