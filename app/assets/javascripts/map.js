923d0784c84794a734108a6cdcd1bc460ff646d5
Wylie Conlon <wylieconlon@gmail.com>
2012-06-10 10:49:23 -0700

    Turned KML into GeoRSS feed

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,14 +3,25 @@
 $(function() {
 	var map = new GMaps({
 		div: '#map',
-		lat: -12.043333,
-		lng: -77.028333,
-		zoom: 1,
+		lat: 37.7827753,
+ 	   	lng: -122.3931977,
+		zoom: 4,
 		mapTypeControl: false,
 		streetViewControl: false
 	});
 
+	infoWindow = new google.maps.InfoWindow({});
+
 	map.loadFromKML({
-		url: 'https://dl.dropbox.com/s/4kuqbbn72wxkvss/companies.kml?dl=1'
+		url: 'https://dl.dropbox.com/s/glfuualox4i0ag9/companies.rss?dl=1',
+		suppressInfoWindows: true,
+		events: {
+			click: function(point){
+				console.log(point, point.featureData.status);
+				infoWindow.setContent(point.featureData.infoWindowHtml);
+				infoWindow.setPosition(point.latLng);
+				infoWindow.open(map.map);
+			}
+		}
 	});
 });
