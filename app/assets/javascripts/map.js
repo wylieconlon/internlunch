13ed32db4728ff3737586809dfc146f42b2121f9
Wylie Conlon <wylieconlon@gmail.com>
2012-06-10 12:00:17 -0700

    Cache busting URL

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -13,7 +13,7 @@ $(function() {
 	infoWindow = new google.maps.InfoWindow({});
 
 	map.loadFromKML({
-		url: 'http://internlunch.herokuapp.com/geofeed',
+		url: 'http://internlunch.herokuapp.com/geofeed/1',
 		suppressInfoWindows: true,
 		events: {
 			click: function(point){
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Internlunch::Application.routes.draw do
   match 'register', :to => 'register#index', :as => :add_user_info_form
   match 'register/create', :to => 'register#create', :as => :add_user_info
 
-  match 'geofeed', :to => 'pages#geofeed'
+  match 'geofeed/:id', :to => 'pages#geofeed'
 
   get 'info' => 'pages#info'
 
